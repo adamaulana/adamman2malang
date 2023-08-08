a09396eae506d3920d745285747cdacdf824447c
adamaulana <adhammaoelana@gmail.com>
2023-08-08 09:47:09 +0700

    Add Restful API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,88 @@ class UserController extends Controller
 
         return redirect('user');
     }
+
+
+    // API CONTROLLER
+    public function get(){
+        $user = User::all();
+
+        return response()->json([
+            'status' => true,
+            'message'=> "berhasil menampilkan data user",
+            'data'   => $user
+        ]);
+    }
+
+    public function store(Request $req){
+        $validator = Validator::make($req->all(), [
+            'nama' => 'required|max:200|alpha:ascii',
+            'usia' => 'required|numeric',
+            'kota' => 'required|alpha:ascii',
+        ]);
+
+
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message'=> "Tidak lulus validasi data",
+            ]);
+        }
+
+        $user =  User::create([
+            'nama' => $req->nama,
+            'usia' => $req->usia,
+            'kota' => $req->kota
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message'=> "berhasil menambahkan data user",
+            'data'   => $user
+        ]);
+
+
+    }
+
+    public function edit(Request $req){
+        $validator = Validator::make($req->all(), [
+            'nama' => 'required|max:200|alpha:ascii',
+            'usia' => 'required|numeric',
+            'kota' => 'required|alpha:ascii',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message'=> "Tidak lulus validasi data",
+            ]);
+        }
+
+        $user = User::where('id',$req->id)->first();
+        $user->nama = $req->nama;
+        $user->usia = $req->usia;
+        $user->kota = $req->kota;
+        $user->save();
+
+        return response()->json([
+            'status' => true,
+            'message'=> "berhasil mengupdate data user",
+            'data'   => $user
+        ]);
+    }
+
+    public function delete(Request $req){
+        $user = User::where('id',$req->id)->delete();
+        return response()->json([
+            'status' => true,
+            'message'=> "berhasil menghapus data",
+            'data'   => $user
+        ]);
+    }
+
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/get',[UserController::class,'get']);
+Route::post('/store',[UserController::class,'store']);
+Route::post('/edit/{id}',[UserController::class,'edit']);
+Route::get('/delete/{id}',[UserController::class,'delete']);
